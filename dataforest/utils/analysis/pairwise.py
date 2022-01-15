4ac1f75fde163d3ab3d308a1598f081789b654ef
Austin McKay <austinmckay303@gmail.com>
2022-01-14 18:17:55 -0700

    Update pairwise.py

--- dataforest/utils/analysis/pairwise.py
+++ dataforest/utils/analysis/pairwise.py
@@ -1,29 +1,28 @@
 from itertools import product
+from typing import Union
 
 import pandas as pd
-from seaborn import heatmap
+from seaborn import heatmap, clustermap
 
 
-def pairwise_metric(args_dict, metric):
-    def _map_df(results_dict):
-        keys = tuple(set.union(*list(map(set, results_dict.keys()))))
-        df = pd.DataFrame(columns=keys, index=keys)
-
-        def map_item(item):
-            df.loc[item[0][0], item[0][1]] = item[1]
-
-        list(map(map_item, results_dict.items()))
-        return df
-
-    results_dict = dict()
-    pair_keys = product(args_dict, args_dict)
+def pairwise_metric(args_map: Union[pd.Series, dict], metric):
+    if isinstance(args_map, dict):
+        args_map = pd.Series(args_map)
+    pair_keys = product(args_map.index.tolist(), args_map.index.tolist())
+    df = pd.DataFrame()
     for keys in pair_keys:
-        pair = list(map(args_dict.__getitem__, keys))
+        pair = args_map[list(keys)].tolist()
         result = metric(*pair)
-        results_dict[keys] = result
-    df = _map_df(results_dict).astype(float)
+        df.loc[keys[0], keys[1]] = result
     return df
 
 
-def pairwise_metric_heat(args_dict, metric, **kwargs):
-    return heatmap(pairwise_metric(args_dict, metric), **kwargs)
+def pairwise_metric_groups(args_dict_1, args_dict_2, metric):
+    """For when there are two separate groups to compare"""
+    raise NotImplementedError()
+
+
+def pairwise_metric_heat(args_dict, metric, cluster=False, **kwargs):
+    plot_func = clustermap if cluster else heatmap
+    corr = pairwise_metric(args_dict, metric)
+    return corr, plot_func(corr, **kwargs)
